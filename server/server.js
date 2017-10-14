f0bf6e928f4710cd7b70e05acfbf1fd7aa2354b0
Phillip Rhinehart <rhinehartphillip@gmail.com>
2017-10-14 14:18:59 -0400

    added webpack bundles to .gitignore and setup for Heroku

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
@@ -9,4 +10,4 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(3000, () => console.log('server is up'));
+app.listen(port, () => console.log('server is up'));
