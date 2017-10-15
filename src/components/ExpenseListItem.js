5f39528782f9b1ce4b5b34f054437457da365e62
Phillip Rhinehart <rhinehartphillip@gmail.com>
2017-10-14 20:05:25 -0400

    added formatting to expense list items

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import numeral from 'numeral';
+import moment from 'moment';
 
 export const ExpenseListItem = ({ id, description, createdAt, amount}) => (
     <div>
         <Link to={'/edit/'+ id}><h3>{description}</h3></Link>
-        <p>${amount/100} - {createdAt}</p>
+        <p>
+            {numeral(amount / 100).format('$0,0.00')}
+             - 
+             {moment(createdAt).format('MMMM Do, YYYY')}
+        </p>
     </div>
 );
 
